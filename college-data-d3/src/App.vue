4f0c3184a127d4cf3e55fba1eaefd5f3aa278a3c
Greg <greg@Gregs-MacBook-Pro.local>
2018-05-23 07:14:31 -0400

    Had to uninstall/reinstall node to get things to work correctly. but here we are

--- college-data-d3/src/App.vue
+++ college-data-d3/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header></header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from "@/components/layout_components/header";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    Header
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ college-data-d3/src/components/app_components/homepage/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="homepage">
+    HELLO WORLD
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+};
+</script>
+
+<style >
+</style>
